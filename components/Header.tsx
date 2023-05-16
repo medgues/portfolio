041b8c4f1b6a10ed8560ab068dde927125933df5
medgues <mohamedlamine.guesmia@univ-tebessa.dz>
2023-05-16 13:41:19 +0100

    fixed hover effect bug on header icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header({}: Props) {
     
   }
   return (
-    <header className="sticky mx-10 sm:mx-24  bg-[#F7AB0A]/10  top-0 px-6 rounded-full flex items-start justify-between max-w-7xl  z-20 xl:items-center">
+    <header className="sticky mx-5 sm:mx-24  bg-[#F7AB0A]/10  top-0 px-6 rounded-full flex items-start justify-between max-w-7xl  z-20 xl:items-center">
       <motion.div
         initial={{
           x: -500,
@@ -37,14 +37,14 @@ export default function Header({}: Props) {
           network="linkedin"
           fgColor="currentColor"
           bgColor=" transparent"
-          className="cursor-pointer hoverShadow hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full pr-2 h-16 w-16 mx-auto object-cover filter group-hover:-translate-y-1 group-hover:scale-125 duration-300 transition ease-in-out"
+          className="cursor-pointer  hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full pr-2 h-16 w-16 mx-auto object-cover filter hover:-translate-y-1 hover:scale-125 duration-300 transition ease-in-out"
         />
         <SocialIcon
           onClick={()=> window.open("https://github.com/medgues")}
           network="github"
           fgColor="currentColor"
           bgColor="transparent"
-          className="cursor-pointer hoverShadow hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full p-2 h-16 w-16 mx-auto object-cover filter group-hover:-translate-y-1 group-hover:scale-125 duration-300 transition ease-in-out"
+          className="cursor-pointer  hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full p-2 h-16 w-16 mx-auto object-cover filter hover:-translate-y-1 hover:scale-125 duration-300 transition ease-in-out"
         />
         <SocialIcon
           // onClick={()=> window.open("https://www.facebook.com/MeDgUes/")}
@@ -52,14 +52,14 @@ export default function Header({}: Props) {
           network="facebook"
           fgColor="currentColor"
           bgColor="transparent"
-          className="cursor-pointer hoverShadow hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full p-2 h-16 w-16 mx-auto object-cover filter group-hover:-translate-y-1 group-hover:scale-125 duration-300 transition ease-in-out"
+          className="cursor-pointer  hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full p-2 h-16 w-16 mx-auto object-cover filter hover:-translate-y-1 hover:scale-125 duration-300 transition ease-in-out"
         />
         <SocialIcon
           onClick={()=> window.open("https://wa.me/+213698179358?text=hey%2C%0Ai%20am%20interested%20in%20your%20services%2C")}
           network="whatsapp"
           fgColor="currentColor"
           bgColor="transparent"
-          className="cursor-pointer hoverShadow hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full p-2 h-16 w-16 mx-auto object-cover filter group-hover:-translate-y-1 group-hover:scale-125 duration-300 transition ease-in-out"
+          className="cursor-pointer  hover:text-[rgba(247,171,10,50%)] text-gray-500 rounded-full p-2 h-16 w-16 mx-auto object-cover filter hover:-translate-y-1 hover:scale-125 duration-300 transition ease-in-out"
         />
       </motion.div>
 
